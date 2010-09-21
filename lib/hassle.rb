4996fea736584d337b4ff18f7e795601ca37ef19
Kevin Moore <kevin@thinkpixellab.com>
2010-09-22 04:24:56 +0800

    Integrating ActionView monkey patch

--- lib/hassle.rb
+++ lib/hassle.rb
@@ -1,2 +1,19 @@
-require File.dirname(__FILE__) + '/hassle/hassle'
-require File.dirname(__FILE__) + '/hassle/railtie' 
+module HassleRails
+  class Railtie < Rails::Railtie
+    initializer :after_initialize do
+      require 'rack'
+      require 'sass'
+      require 'sass/plugin'
+
+      if (ENV.include?('HEROKU_UPID'))
+        require File.dirname(__FILE__) + '/hassle/hassle'
+        if defined? Rails.configuration && defined? Rails.configuration.middleware
+          Rails.configuration.middleware.use(Hassle)
+        elsif defined? ActionController::Dispatcher && defined? ActionController::Dispatcher.middleware
+          ActionController::Dispatcher.middleware.use Hassle
+        end
+        require File.dirname(__FILE__) + '/hassle/actionview'
+      end
+    end
+  end
+end
